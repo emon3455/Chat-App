d1395d5ab24e3e7d9c7703bbac57bc6baadd0469
Emon <emon.mhk69@gmail.com>
2023-04-21 20:00:17 +0600

    deployment done

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -23,7 +23,7 @@ const NavigationBar = () => {
     return (
         <div className='navbar fixed z-10 bg-neutral text-neutral-content'>
             <div className="conatinerWrap flex justify-between">
-                <a className="btn btn-ghost normal-case text-xl">Instant Chat</a>
+                <a className="btn btn-ghost normal-case text-2xl font-semibold">Let's Chat</a>
                 <button onClick={handleLogOut}>Log Out</button>
             </div>
         </div>
